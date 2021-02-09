8e5b721b072ea51ee8e574220a975223513afdcb
shieldsy71 <tom.shields@g.austincc.edu>
2021-02-09 10:51:35 -0600

    Monday 8th

--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -1,8 +1,10 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include"../src/examples/02_module/01_expressions/expressions.h"
+#include "casting.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
+TEST_CASE("Verify Test Configuration", "verification") 
+{
 	REQUIRE(true == true);
 }
 
@@ -21,12 +23,21 @@ TEST_CASE("Verify op prec 1")
 
 /*Test case convert double to int to show that a double converted to int truncates 
 the decimal portion*/
-
+TEST_CASE("Test convert to double", "int * double, int converted to double")
+{
+	REQUIRE(convert_to_double(10, .35)==3.5);
+}
 /*Test case static cast double int to show that a double casted to int truncates
 decimal portion*/
-
+TEST_CASE("Test convert to double", "int * double, int converted to double")
+{
+	REQUIRE(static_cast double_int(10.375)==10);
+}
 /*Test int overflow to show adding 1 to 2147483647 returns -2147483648*/
-
+TEST_CASE("Test int overflow", "Create C++ weirdness")
+{
+	REQUIRE(int_overflow()==2147483648);
+}
 /*Test get radius of a circle with 10 to show that it returns 314.159*/
 
 /*Test int data size */
