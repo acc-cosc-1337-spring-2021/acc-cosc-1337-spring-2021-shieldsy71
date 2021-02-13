250a24d19eeaba4e64472cfad10b0e0fc025c62f
shieldsy71 <tom.shields@g.austincc.edu>
2021-02-12 18:40:32 -0600

    HW 2 edit 3

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -19,26 +19,20 @@ int main()
 
 	cout<<"Enter the meal amount";
 	cin>>meal_amount;
-
 	
 	tax_amount =  get_sales_tax_amount(meal_amount);
-	
-	
 
 	cout<<"Enter the tip amount";
 	cin>>tip_rate;
 
 	tip_amount = get_tip_amount(meal_amount, tip_rate);
 	
-
-	
-
 	double total = tip_amount + tax_amount + meal_amount;
 
-	cout<<"Meal amount:	"<<meal_amount;
-	cout<<"Sales Tax:	"<<tax_amount;
-	cout<<"Tip Amount:	"<<tip_amount;
-	cout<<"Total:	"<<total;
+	cout<<"Meal amount:	"<<meal_amount<<"\n";
+	cout<<"Sales Tax:	"<<tax_amount<<"\n";
+	cout<<"Tip Amount:	"<<tip_amount<<"\n";
+	cout<<"Total:	"<<total<<"\n";
 
-
+	return 0;
 }
