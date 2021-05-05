be9ea480e1af4379bbea1fda86942a99ca7fd2ca
shieldsy71 <tom.shields@g.austincc.edu>
2021-05-05 16:33:09 -0500

    HW 3 Test Case Edit

--- test/homework_test/03_decisions_test/03_decisions_tests.cpp
+++ test/homework_test/03_decisions_test/03_decisions_tests.cpp
@@ -9,6 +9,10 @@ TEST_CASE("Verify Test Configuration", "verification") {
 TEST_CASE("Test numeric grade function", "return letter grade")
 {
 	REQUIRE(get_letter_grade_using_if(93)=="A");
+	REQUIRE(get_letter_grade_using_if(83)=="B");
+	REQUIRE(get_letter_grade_using_if(73)=="C");
+	REQUIRE(get_letter_grade_using_if(63)=="D");
+	REQUIRE(get_letter_grade_using_if(53)=="F");
 }
 
 
@@ -16,4 +20,8 @@ TEST_CASE("Test numeric grade function", "return letter grade")
 TEST_CASE("Test numeric grade using switch", "returns letter grade")
 {
 	REQUIRE(get_letter_grade_using_switch(92) == "A");
+	REQUIRE(get_letter_grade_using_switch(82) == "B");
+	REQUIRE(get_letter_grade_using_switch(72) == "C");
+	REQUIRE(get_letter_grade_using_switch(62) == "D");
+	REQUIRE(get_letter_grade_using_switch(52) == "F");
 }
